153793d7bb18a1f90d8eeb9ffa13682220918cc1
Nathan Scott <natscott@uat.edu>
2018-10-12 16:31:55 -0700

    Tree class member variables and function headers

--- nscott_csc382_binarysearchtree/nscott_csc382_binarysearchtree/BSTree.h
+++ nscott_csc382_binarysearchtree/nscott_csc382_binarysearchtree/BSTree.h
@@ -11,9 +11,44 @@ template<typename Type>
 class BSTree
 {
 private:
-
+	BSNode<Type>* rootPtr = nullptr;		// Pointer to the rootPtr node of this tree
 public:
+	BSTree() {}		// Constructor
+	~BSTree() {}	// Destructor
 
+	BSNode<Type>* GetRootPtr()		// Returns the memory address of the root node
+	{
+
+	}
+
+	template<typename Type>
+	void SetRootPtr(Type* newPtr)		// Changes which node is the tree's root node
+	{
+
+	}
+
+	template<typename Type>
+	BSNode<Type>* CreateNode(Type nodeValue)	// Creates a new node in the tree
+	{
+
+	}
+
+	template<typename Type>
+	BSNode<Type>* FindNode(Type findValue)	// Attempts to find a node containing the given value
+	{
+
+	}
+
+	template<typename Type>
+	void DeleteNode(BSNode<Type>* nodePtr)	// Deletes a node from the tree
+	{
+
+	}
+
+	Type MaxValue()		// Finds the largest value stored in the tree
+	{
+
+	}
 };
 
 #endif
